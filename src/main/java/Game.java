968baf002257e2379c6abd82e7c615b55a3b1fce
Anastasiia Fokina <stasechka11@yandex.ru>
2024-01-10 13:19:15 +0300

    Added Player and Game initial realization

---
+++ src/main/java/Game.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Game {
+    List<Player> playersList = new ArrayList<>();
+
+    public void register(Player player) {
+        playersList.add(player);
+    }
+}
